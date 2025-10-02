f702462050681e7e9f6c7eec4a9a4563decc6808
omerfuyar <denemeyo@gmail.com>
2025-10-02 12:35:14 +0300

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 "use strict";
-//const API_URL = 'https://patrice-trypanosomal-sherryl.ngrok-free.dev';
-//const API_URL = 'https://code-foxtrot.onrender.com';
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://patrice-trypanosomal-sherryl.ngrok-free.dev';
 const rootElement = document.getElementById('root');
 if (rootElement) {
     const textElement = document.createElement('div');
